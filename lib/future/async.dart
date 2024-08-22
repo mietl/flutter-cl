a0326c18b5f898af84cfc79e538c26e7013a8328
mietl <mietl.moe@icloud.com>
2024-08-22 22:59:29 +0800

    future

---
+++ lib/future/async.dart
@@ -0,0 +1,28 @@
+Future <String> fetchUserOrder1()=> Future.delayed(const Duration(seconds: 1),()=>'Order 1');
+
+Future<String> fetchUserOrder2()=> Future.delayed(const Duration(seconds: 2),()=>'Order2');
+
+Future<String> fetchUserOrder3()=> Future.delayed(const Duration(seconds: 3),()=>'Order3');
+
+
+void main(List<String> args) {
+  doTasks();
+}
+
+// 并行执行
+Future<void> doTasks() async{
+  var startTime = DateTime.now().second;
+
+  var order1 = fetchUserOrder1();
+  var order2 = fetchUserOrder2();
+  var order3 = fetchUserOrder3();
+
+  await order1;
+  await order2;
+  await order3;
+
+  // 三个任务同时进行，当任务3结束时，任务2 早已经执行完了，所以结果是3秒
+
+  var endTime = DateTime.now().second;
+  print(endTime - startTime); // >  3s
+}
